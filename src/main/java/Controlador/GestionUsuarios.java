ff822bf947b138262a6b9e70b26ff3e626994e3d
ArduAndroid <109stuka@gmail.com>
2024-12-07 19:23:47 +0100

    login implementado

--- src/main/java/Controlador/GestionUsuarios.java
+++ src/main/java/Controlador/GestionUsuarios.java
@@ -1,16 +1,36 @@
 package Controlador;
 
 import Modelo.DAOGenerico;
+import Modelo.DAOUsuario;
 import Modelo.Usuario;
 
+import java.util.Scanner;
+
 public class GestionUsuarios {
-    public DAOGenerico<Usuario,Integer> daousuario;
+    public DAOUsuario daousuario;
+    String dni;
+    String password;
 
     public GestionUsuarios(){
-        this.daousuario = new DAOGenerico<>(Usuario.class,Integer.class);
+        this.daousuario = new DAOUsuario();
     }
-    public boolean login(){
-        return false;
+
+    public boolean login(String dni, String password){
+        Usuario usu = daousuario.getUsuarioByDni(dni);
+        if(usu.getPassword().equals(password)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void pedirDatos(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce el DNI: ");
+        this.dni = teclado.nextLine();
+        System.out.print("Introduce la contraseña: ");
+        this.password = teclado.nextLine();
     }
 
 }
